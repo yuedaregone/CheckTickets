be8107175f20dd4160daee2666deb4a504aac7ba
yuedaregone <810566249@qq.com>
2016-01-07 20:40:59 +0800

    das

--- src/CheckTicket.py
+++ src/CheckTicket.py
@@ -5,6 +5,7 @@ import json
 import Tkinter
 import tkMessageBox
 import time
+import datetime
 
 def show(msg):
     tkMessageBox.showinfo(title='tickets', message=msg)
@@ -23,13 +24,22 @@ def getTicketInfo():
 	data = urllib2.urlopen(url).read()
 	json_data = json.loads(data)
 	list_data = json_data["data"]["datas"]
-
-	for item in list_data:
-		print(item["from_station_name"] + ':' + item["yw_num"])
+	
+	now = datetime.datetime.now()
+	now.strftime('%Y-%m-%d %H:%M:%S')
+	print("-------------------")
+	print(now)
+	print("-------------------")
+	for item in list_data:		
+		print(item["from_station_name"])
+		print(u"硬卧:" + item["yw_num"])
 		if isHasTicks(item["yw_num"]):
 			show(item["from_station_name"] + ':' + item["yw_num"])
-
+		
 while 1:
-	getTicketInfo()
-	time.sleep(2)
+	try:
+		getTicketInfo()
+	except:
+		print("Get An Except")
+	time.sleep(1)
 
