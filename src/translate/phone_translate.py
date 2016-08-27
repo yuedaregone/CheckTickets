c248821207e37922fe6b01f6d8083469df4e5186
yuedaregone <810566249@qq.com>
2016-08-27 16:30:59 +0800

    add local ch dict

--- src/translate/phone_translate.py
+++ src/translate/phone_translate.py
@@ -9,6 +9,7 @@ import sqlite3
 
 request_url = 'http://fanyi.youdao.com/openapi.do?keyfrom=cmd-translation&key=50036382&type=data&doctype=json&version=1.1&q='
 dict_path = "/yhDict.db"
+conn_inst = None
 
 def  get_html_content(url):	
 	content = ''
@@ -18,16 +19,20 @@ def  get_html_content(url):
 		print "get html error!"
 	return content
 
-def load_local_translate(word):
-	conn = sqlite3.connect(dict_path)
-	command =  "SELECT explain FROM Words WHERE word='%s'" % word
-	cursor = conn.execute(command)
+def load_local_translate(word, is_ch):
+	if conn_inst == None:
+		return None	
+	command = ""
+	if is_ch == True:
+		command = "SELECT explain FROM Translate WHERE word='%s'" % word
+	else:
+		command = "SELECT explain FROM Words WHERE word='%s'" % word
+	cursor = conn_inst.execute(command)
 	result = ()
 	for cown in cursor:
-		result = result + cown		
-	conn.close()
+		result = result + cown	
 
-	if len(result) == 0:
+	if len(result) == 0:		
 		return None
 	return [("本地释义", result)]
 
@@ -56,12 +61,34 @@ def fecth_net_translate(word):
 	return result	
 
 def has_chinese_character(t_str):
-	zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
+	zhPattern = re.compile('[^\u4e00-\u9fa5]+')
 	match = zhPattern.search(t_str)
 	if match:
 		return True
 	else:
 		return False
+
+def insert_translate(key, fetch_data, is_ch):
+	if conn_inst == None:
+		return	
+	explain = ""
+	for item in fetch_data:		
+		if item[1] == None:
+			break
+		if type(item[1]) == list or type(item[1]) == tuple:
+			for exp in item[1]:				
+				explain = explain + exp + ","
+		else:
+			explain = explain + item[1] + ","
+	if explain == "":
+		return
+	command = ""	
+	if is_ch == True:
+		command = "INSERT INTO Translate VALUES ('%s', '%s')" % (key.decode("utf8"), explain)
+	else:
+		command = "INSERT INTO Words VALUES ('%s', '%s')" % (key.decode("utf8"), explain)	
+	conn_inst.execute(command)
+	conn_inst.commit()
 
 if __name__ == '__main__':
 	if len(sys.argv) <= 1:
@@ -69,15 +96,24 @@ if __name__ == '__main__':
 		sys.exit()
 
 	dirname,fileName = os.path.split(os.path.abspath(sys.argv[0]))
-	dict_path = dirname + dict_path
+	dict_path = dirname + dict_path	
 
 	keyword = sys.argv[1]
-	result = None
-	if os.path.exists(dict_path) and not has_chinese_character(keyword):
-		result = load_local_translate(keyword)
+	isHasChinese = has_chinese_character(keyword)
+	#print(isHasChinese)
+	#sys.exit()
 
+	if os.path.exists(dict_path):
+		conn_inst = sqlite3.connect(dict_path)
+
+	result = None
+	if conn_inst != None:
+		result = load_local_translate(keyword, isHasChinese)
+
+	is_from_net = False
 	if result == None:
 		result = fecth_net_translate(keyword)
+		is_from_net = True
 
 	if result == None:
 		print("没有找到翻译")
@@ -93,5 +129,11 @@ if __name__ == '__main__':
 			continue
 		else:
 			print(k[1].encode("utf8"))
+	
+	if is_from_net == True:
+		insert_translate(keyword, result, isHasChinese)
+
+	if conn_inst != None:
+		conn_inst.close()
 		
 
