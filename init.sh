de89f9edc918d0d41228c886a5ea238fd6bcbaff
yuedaregone <810566249@qq.com>
2016-09-28 19:48:36 +0800

    addd

--- init.sh
+++ init.sh
@@ -5,7 +5,6 @@ lua_folder=lua-5.3.3
 lua_pkg_name=$lua_folder.tar.gz
 lua_url=http://www.lua.org/ftp/$lua_pkg_name
 
-
 ### init env
 #sudo apt update
 #sudo apt upgrade
@@ -13,6 +12,8 @@ lua_url=http://www.lua.org/ftp/$lua_pkg_name
 cd ~
 mkdir -p $pkg_folder
 cd $pkg_folder
+###
+
 
 ### Lua5.3.3
 wget -O $lua_pkg_name $lua_url
@@ -20,8 +21,5 @@ tar -C ../ -zxvf $lua_pkg_name
 cd ../$lua_folder
 make linux
 sudo make install
-
 ###
-
-
 
